d4b22a0812c6917d5cee6c23d94221cb4318db4c
ala decoste <alshareefialaa@gmail.com>
2020-11-14 21:16:04 -0500

    Added certifications and contact info.

---
+++ script.js
@@ -0,0 +1,38 @@
+$(document).ready(function () {
+    //on click submit button
+    $('#btn1').on("click", function (event) {
+        console.log('clicked!');
+        //The coode for submitting a form is from:
+        //https://www.youtube.com/watch?v=eg4e-FObyJ8
+
+        let email = $('#email').val();
+        let subject = $('#subject').val();
+        let message = $('#exampleFormControlTextarea1').val();
+        let statusELM = $('.status');
+        statusELM.empty();
+        //validating the form before submitting
+        if (email.length > 5 && email.includes('@') && email.includes('.')) {
+            console.log('Email is valid.')
+            $("#e-mail").hide();
+        } else {
+            event.preventDefault();
+            $('#e-mail').text('Email is not valid!')
+        }
+
+        if (subject.length >= 2) {
+            console.log('Subject is valid.')
+            $("#sub").hide();
+        } else {
+            event.preventDefault();
+            $('#sub').text('Subject is not valid!');
+        }
+
+        if (message.length >= 10) {
+            console.log('Message is valid.')
+            $("#text").hide();
+        } else {
+            event.preventDefault();
+            $('#text').text('Message is not valid!')
+        }
+    })
+});
